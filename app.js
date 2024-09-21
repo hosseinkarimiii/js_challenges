8457ddb780157836c25a75b181498953cdb16f35
hoseinkarimiii <142800304+hosseinkarimiii@users.noreply.github.com>
2024-09-21 10:41:03 +0330

    new challenges

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 "usestrict";
-//variables and Data types
+//100 javascript challenges, beginner to advanced
+
+//beginner:
 
 //chllaenge 1
 // const input = function (x, y) {
@@ -19,8 +21,6 @@
 // }
 // console.log(num); //output:?
 
-//functions
-
 //chllenge 3
 // const arrow = (name) => name.toLocaleUpperCase();
 // console.log(arrow("hosseinkarimi"));
@@ -29,8 +29,7 @@
 // const fun = numbers.filter((number) => number % 2 !== 0);
 // console.log(fun);
 
-//Loops and Conditionals
-//challenge 1
+//challenge 5
 
 // const loope = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -38,22 +37,34 @@
 //   console.log(loope[i]);
 // }
 
-//challenge 2
-function isprime(num) {
-  if (num <= 1) return false;
-  if (num === 2) return true;
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) return false;
-  }
-  return true;
-}
-console.log(isprime(8), isprime(7));
-function thisprime(prime){
-//   If num is less than or equal to 1, the function returns false because numbers less than or equal to 1 are not prime.
-// If num is exactly 2, the function returns true because 2 is the smallest and only even prime number.
-  if(prime<=1)
-}
-//chllenge 3
-//challenge 4
-//chllenge 5
-//Advanced challenges.
+//challenge 6
+// function isprime(num) {
+//   if (num <= 1) return false;
+//   if (num === 2) return true;
+//   for (let i = 2; i <= Math.sqrt(num); i++) {
+//     if (num % i === 0) return false;
+//   }
+//   return true;
+// }
+// console.log(isprime(8), isprime(7));
+
+//chllenge 7,Create a function that converts minutes to seconds.
+// function convert(minutes) {
+//   return minutes * 60;
+// }
+// const output = convert(4);
+// console.log(output);output??
+
+//challenge 8, Write a function to reverse a string.
+
+// function reverse(str) {
+//   const reversestring = str.split("").reverse("").join("");
+//   console.log(reversestring);
+// }
+// reverse("hello world");
+
+//chllenge 9, 6.	Create a function that returns the first element of an array.
+
+//intermedate challenges:
+
+//Advanced challenges:
